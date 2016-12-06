e613e8ab6fe4251d4c1a62d7927a9b87801b9c70
uestczhuhao <uestczhuhao@163.com>
2016-12-06 19:34:50 +0800

    增加分类

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	models.AddUserSup("zhu", "123456")
 	models.AddUserCom("hao", "123456")
 
-	var err error
+	// var err error
 	// err = models.AddEmployee("aaa", "某某职位qw", "22333322", "843728@123.com", "", "", "1")
 	// err = models.AddEmployee("adddaa", "某某qwq职位", "22333322", "843728@123.com", "", "", "1", "jixie")
 	// err = models.AddEmployee("a", "某某职eqe位", "22333322", "843728@123.com", "", "", "1", "jixie")
@@ -30,15 +30,17 @@ func main() {
 	// 	beego.Error(err)
 	// }
 
-	emps, err := models.SelectEmployeeByDepartment("jixie")
-	// beego.Debug(len(emps))
-
-	for i := 0; i < len(emps); i = i + 1 {
-		beego.Debug(emps[i].Name)
-	}
-	// beego.Debug(emps)
-	if err != nil {
-		beego.Error(err)
-	}
+	// IsError := models.DelCategory(12, "寄语11")
+	// beego.Debug(strings.Replace("a,,d,1,2", ",", "", -1))
+	// emps, err := models.SelectEmployeeByDepartment("jixie")
+	// // beego.Debug(len(emps))
+	//
+	// for i := 0; i < len(emps); i = i + 1 {
+	// 	beego.Debug(emps[i].Name)
+	// }
+	// // beego.Debug(emps)
+	// if err != nil {
+	// 	beego.Error(err)
+	// }
 	beego.Run()
 }
--- models/category.go
+++ models/category.go
@@ -1,5 +1,87 @@
 package models
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/astaxie/beego/orm"
+)
+
 type Category struct {
-	Id int
+	Id       int
+	Name     string `orm:"size(45)"`
+	ParentID string `orm:"size(45)"`
+	ChildID  string `orm:"size(100)"`
+	Depth    int
+	Path     string
+}
+
+func AddCategory(name string, parentid string) error {
+	var err error
+	var desid string
+	var resultid int
+	var curid int64
+	o := orm.NewOrm()
+
+	if strings.Contains(parentid, ",") == true {
+		temp := strings.Split(parentid, ",")
+		// beego.Debug(len(temp))
+		for i := 0; i < len(temp); i++ {
+			desid = temp[i]
+		}
+	} else {
+		desid = parentid
+	}
+	resultid, _ = strconv.Atoi(desid)
+
+	parentcate := Category{Id: resultid}
+
+	if o.Read(&parentcate, "Id") == nil {
+		depth := parentcate.Depth + 1
+
+		path := parentcate.Path + "/" + name
+
+		cur := &Category{Name: name, ParentID: parentid, ChildID: "0", Depth: depth, Path: path}
+
+		curid, err = o.Insert(cur)
+		if err != nil {
+			return err
+		}
+	}
+
+	if o.Read(&parentcate, "Id") == nil {
+		var newchildid string
+		var curidstr string
+		curidstr = strconv.FormatInt(curid, 10)
+
+		if parentcate.ChildID != "0" {
+			newchildid = parentcate.ChildID + "," + curidstr
+			parentcate.ChildID = newchildid
+			o.Update(&parentcate, "ChildID")
+		} else {
+			parentcate.ChildID = curidstr
+			o.Update(&parentcate, "ChildID")
+		}
+	}
+	return err
+}
+
+func DelCategory(id int, name string) int {
+	o := orm.NewOrm()
+	q := o.QueryTable("Category")
+	var category Category
+	// var idstr string
+
+	err := q.Filter("Id", id).Filter("Name", name).One(&category)
+	if err != nil {
+		return 1
+	} else {
+		if category.ChildID == "0" {
+			o.Delete(&category)
+			return 0
+		} else {
+			return 1
+		}
+	}
+
 }
