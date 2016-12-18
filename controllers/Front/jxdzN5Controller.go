6d4dfbb70771756c704a0b7953129652b56fea12
uestczhuhao <uestczhuhao@163.com>
2016-12-18 23:51:43 +0800

    修改了在主页点击相关选项时面包屑和左侧菜单的一些bug

--- controllers/Front/jxdzN5Controller.go
+++ controllers/Front/jxdzN5Controller.go
@@ -15,7 +15,6 @@ func (c *JxdzN5Controller) Get() {
 	if err != nil {
 		beego.Error(err)
 	}
-	beego.Debug(id)
 	models.UpdateArticle(id)
 	article, err := models.SearchArticleById(id)
 	c.Data["Article"] = article
@@ -27,12 +26,13 @@ func (c *JxdzN5Controller) Get() {
 	c.Data["CatesForMenu"], err = models.CreateCateList()
 	c.Data["link"] = models.GetAllLinker()
 
-	c.Data["Path"] = models.FindAllFather(id)
 	beid, _ := c.GetInt("BeId")
 	becate, _ := models.SearchCategory(beid)
 	if becate.Depth > 10 {
+		c.Data["Path"] = models.FindAllFather(beid)
 		c.Data["LeftMenu"] = models.FindLeftMenu(beid)
 	} else {
+		c.Data["Path"] = models.FindAllFather(id)
 		c.Data["LeftMenu"] = models.FindLeftMenu(id)
 
 	}
